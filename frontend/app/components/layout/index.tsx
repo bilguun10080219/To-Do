bc3f618dd23ebde23a570e0fcc50085ee474fb55
Nyambayar <tab.erd.nyambayar@gmail.com>
2025-09-25 14:48:00 +0800

    add components

--- frontend/app/components/layout/index.tsx
+++ frontend/app/components/layout/index.tsx
@@ -1,21 +1,30 @@
 // app/components/layout/index.tsx
 import React, { ReactNode } from "react";
+import cn from "classnames";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 
 interface LayoutProps {
   children: ReactNode;
+  className?: string;
 }
 
-export default function Layout({ children }: LayoutProps) {
+export default function Layout({ children, className }: LayoutProps) {
   return (
-    <div className="flex flex-col min-w-screen gap-[57px] bg-gray-100">
+    <div
+      className={cn(
+        "flex flex-col min-w-screen gap-[57px] bg-gray-100",
+        className
+      )}
+    >
       <Header />
 
       <div className="flex-1 flex flex-row">
         <Sidebar />
-        <div className="flex flex-col justify-between">
-          <main className="flex flex-row w-full p-8">{children}</main>
+        <div className="flex flex-col flex-1 justify-between">
+          <main className="flex flex-col items-start gap-3 p-4">
+            {children}
+          </main>
         </div>
       </div>
     </div>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,25 @@
+import { LogOut } from "lucide-react";
+import Button from "./components/core/Button";
 import Layout from "./components/layout";
+import Input from "./components/core/Input";
+import Status from "./components/core/Status";
 
 // app/page.tsx
 export default function Page() {
   return (
-    <div className="flex-1">
-      <Layout>a</Layout>
+    <div>
+      <Layout>
+        <Button>button</Button>
+        <Button leftIcon={<LogOut />}>Log out</Button>
+        <Button disabled>button</Button>
+        <Button variant="secondary">button</Button>
+        <Button variant="danger">button</Button>
+        <Input />
+
+        <Status status="completed" />
+        <Status status="in-progress" />
+        <Status status="not-completed" />
+      </Layout>
     </div>
   );
 }
